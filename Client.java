f93187d095b0a25c6ce559f1aaa5f58c3848ef9d
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2023-09-17 09:58:48 -0400

    Create Sobel class

--- Client.java
+++ Client.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class Client {
+
+	private void createImage(BufferedImage image) throws Exception{
+			File outputfile = new File("sobel.png");
+				ImageIO.write(image, "png", outputfile);
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
